4f03dd4550f246808c28d212516e6193e8509deb
Jeff <mason8971@gmail.com>
2024-02-09 12:35:41 -0500

    update efc

--- src/extractFeature2csv.cpp
+++ src/extractFeature2csv.cpp
@@ -1,5 +1,5 @@
 /**
- * @file extractImgFeature2csv.cpp
+ * @file extractFeature2csv.cpp
  * @author Yuan Zhao (zhao.yuan2@northeatern.edu)
  * @brief main entry for the project
  * @version 0.1
@@ -27,7 +27,7 @@ void Menu(){
 
 int main(int argc, char* argv[]){
     // Check the number of arguments
-    if (argc < 4) {
+    if (argc < 3) {
         std::cerr << "Usage: " << argv[0] << " <method> <directory_of_images>" << std::endl;
         Menu();
         return EXIT_FAILURE;
@@ -41,21 +41,8 @@ int main(int argc, char* argv[]){
     }
 
     // Set the directory path from the command line, 2nd and 3rd argument
-    std::string target_image_name = std::string(argv[2]);
-    std::string image_directory_path = std::string(argv[3]);
-
-    // Set the number of top N images to output
-    int N = 4;
-    if (argc == 5) {
-        N = std::stoi(argv[4]);
-    }
-    std::cout << "N is set to " << N << std::endl;
-    // Check if N is valid
-    if (N < 1) {
-        std::cerr << "Error: invalid N" << std::endl;
-        return EXIT_FAILURE;
-    }
-
+    std::string method = std::string(argv[1]);
+    std::string directory_of_images = std::string(argv[2]);
 
     // Set the csv file name
     std::string csvFile;
@@ -74,38 +61,19 @@ int main(int argc, char* argv[]){
         return EXIT_FAILURE;
     }
 
-
-    // read the target image
-    std::string target_image_directory_path = image_directory_path + "/" + target_image_name;
-
-
-
-
-    // Delete the existing file 
-    std::remove(csvFile.c_str());
-
-    // Read the target image
-    cv::Mat target_image = cv::imread(target_image_directory_path, cv::IMREAD_COLOR);
-    if (target_image.empty()) {
-        std::cerr << "Error: cannot read image " << target_image_directory_path << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    // Extract the feature vector from the target image
-    std::vector<float> target_feature = extract7x7FeatureVector(target_image);
     
     // Read the images from the directory
     std::vector<std::pair<std::string, float>> image_distance_pairs;
     DIR *dir;
     struct dirent* ent;
 
-    if ((dir = opendir(image_directory_path.c_str())) != NULL) {
+    if ((dir = opendir(directory_of_images.c_str())) != NULL) {
         while ((ent = readdir(dir)) != NULL) {
             std::string file_name = ent->d_name;
             // Skip current directory and parent directory entries
             if (file_name == "." || file_name == "..") continue;
 
-            std::string full_file_path = image_directory_path + "/" + file_name;
+            std::string full_file_path = directory_of_images + "/" + file_name;
             std::string extension = file_name.substr(file_name.find_last_of(".") + 1);
 
             // Skip files that are not images
@@ -126,22 +94,10 @@ int main(int argc, char* argv[]){
         }
         closedir(dir);
     } else {
-        std::cerr << "Error: cannot open directory " << image_directory_path << std::endl;
+        std::cerr << "Error: cannot open directory " << directory_of_images << std::endl;
         return EXIT_FAILURE;
     }
 
-    // Sort the image-distance pairs by distance
-    std::sort (image_distance_pairs.begin(), image_distance_pairs.end(), 
-        [](const std::pair<std::string, float> &a, const std::pair<std::string, float> &b) -> bool {
-            return a.second < b.second;
-        }
-    );
 
-    // Output the top N images and their distances
-    for (int i = 0; i < (N + 1); i++) {
-        std::cout << "Image: " << image_distance_pairs[i].first << " Distance: " << image_distance_pairs[i].second << std::endl;
-    }
-
-    return 0;
 
 }
